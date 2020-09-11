83e98addf0e9a35cc5400d5ee7acc638e5e2eba5
Shahrukh Muhammad Qasim <ShahrukhMuhammad.Qasim@gfk.com>
2020-09-11 16:40:14 +0500

    Add 23

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TestGitDemo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. Test 1";
+            ViewBag.Message = "Your application description page. Test 1 23";
 
             return View();
         }
